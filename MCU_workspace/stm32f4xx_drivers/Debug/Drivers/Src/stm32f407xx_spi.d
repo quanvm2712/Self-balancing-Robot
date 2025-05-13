d91abe21a03d227ad28b28f6dd62c5598bf0cb00
duongn776 <duongn776@gmail.com>
2025-05-13 19:19:21 +0700

    update second time

--- MCU_workspace/stm32f4xx_drivers/Debug/Drivers/Src/stm32f407xx_spi.d
+++ MCU_workspace/stm32f4xx_drivers/Debug/Drivers/Src/stm32f407xx_spi.d
@@ -1,21 +1,21 @@
 Drivers/Src/stm32f407xx_spi.o: ../Drivers/Src/stm32f407xx_spi.c \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/aht20_sensor.h \
- D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/aht20_sensor.h:
-D:/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
