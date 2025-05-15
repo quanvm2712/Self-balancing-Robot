d82ea80523a303ae556c6ecd4a804aae930ce7da
duongn776 <duongn776@gmail.com>
2025-05-15 20:19:33 +0700

    update main

--- MCU_workspace/stm32f4xx_drivers/Debug/Drivers/Src/delay.d
+++ MCU_workspace/stm32f4xx_drivers/Debug/Drivers/Src/delay.d
@@ -1,23 +1,23 @@
 Drivers/Src/delay.o: ../Drivers/Src/delay.c \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/SysTick.h \
- D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/SysTick.h:
-D:/Project/Self_Balancing_Robot/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/SysTick.h \
+ D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_usart.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_rcc.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_tim.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/SysTick.h:
+D:/Project/Self-balancing_1/MCU_workspace/stm32f4xx_drivers/Drivers/Inc/delay.h:
